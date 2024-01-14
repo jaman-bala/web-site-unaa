6744f7aa4f2c6cacae42a353a45de9c6d7ced7ce
jaman-bala <ermekoffdastan@yandex.ru>
2024-01-14 22:05:39 +0600

    add new new rules for the admin panel

--- src/admin/views.py
+++ src/admin/views.py
@@ -1,13 +1,34 @@
-from sqladmin import ModelView
+from sqladmin import ModelView, BaseView
 
 from auth.models import User
 from news.models import News
 from informations.models import Information
 
 
-class UserAdmin(ModelView, model=User):
-    column_list = [User.id, User.email, User.username, User.is_active]
+class UserAdmin(ModelView, BaseView, model=User):
+    def date_format(value):
+        return value.strftime("%d.%m.%Y")
+
+    form_columns = "__all__" # Можно сделать чтоб форма открывалось User.username
+
+    column_list = [
+        User.id,
+        User.email,
+        User.username,
+        User.role_id,
+        User.is_active
+    ]
+    column_searchable_list = [User.email, User.username]
+    column_sortable_list = [User.id]
+
+    column_labels = {
+        User.email: "Email",
+        User.username: "Пользователь",
+        User.role_id: "Роль",
+        User.is_active: "Активный"
+    }
     column_details_exclude_list = [User.hashed_password]
+
     name = "Пользователь"
     name_plural = "Пользователи"
     icon = "fa-solid fa-user"
@@ -15,16 +36,45 @@ class UserAdmin(ModelView, model=User):
 
 
 class NewsAdmin(ModelView, model=News):
-    column_list = [News.id, News.title, News.publication_date]
+    def date_format(value):
+        return value.strftime("%d.%m.%Y")
+
+    column_list = [
+        News.id,
+        News.title,
+        News.img,
+        News.file,
+        News.publication_date
+    ]
+    column_searchable_list = [News.title]
+    column_labels = {
+        News.title: "Наименование",
+        News.img: "Изображение",
+        News.file: "Файл",
+        News.publication_date: "Дата публикации",
+        News.description: "Описание"
+    }
+
     name = "Новости"
     name_plural = "Новости"
     icon = "fa-solid fa-newspaper"
     category = "Новости"
 
+    can_create = True
+    can_edit = True
+    can_delete = False
+    can_view_details = True
+
 
 class InformationAdmin(ModelView, model=Information):
-    column_list = [Information.id, Information.paragraph, Information.title, Information.money]
+
+    column_list = "__all__"
     name = "Информация"
     name_plural = "Информация"
     icon = "fa-solid fa-info-circle"
     category = "Информация"
+
+    can_create = True
+    can_edit = True
+    can_delete = False
+    can_view_details = True
--- src/news/models.py
+++ src/news/models.py
@@ -1,7 +1,11 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime, func, TIMESTAMP, MetaData, Table
+from fastapi_storages import FileSystemStorage
+from fastapi_storages.integrations.sqlalchemy import FileType, ImageType
 from database import Base
 
 metadata = MetaData()
+storage_img = FileSystemStorage(path="./static/images")
+storage_file = FileSystemStorage(path="./static/file")
 
 news = Table(
     "news",
@@ -9,7 +13,8 @@ news = Table(
     Column("id", Integer, primary_key=True),
     Column("title", String),
     Column("description", Text),
-    Column("img", String, nullable=True),
+    Column("img", ImageType(storage=storage_img), nullable=True),
+    Column("file", FileType(storage=storage_file), nullable=True),
     Column("publication_date", TIMESTAMP),
 )
 
@@ -20,7 +25,8 @@ class News(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(Text)
-    img = Column(String, nullable=True)
+    img = Column(ImageType(storage=storage_img), nullable=True)
+    file = Column(FileType(storage=storage_file), nullable=True)
     publication_date = Column(DateTime(timezone=True), server_default=func.now())
 
     def __str__(self):
